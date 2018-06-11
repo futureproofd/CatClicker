15cc4d6be8d5e8544eaab3e6ba1188f2854d51c0
Marcus <nexus06@gmail.com>
2018-06-11 14:28:36 -0400

    Changed to simpler DOM selection
    -reuse html elements
    -reduce some model complexity

--- CatClicker/scripts/app.js
+++ CatClicker/scripts/app.js
@@ -1,118 +1,126 @@
-$(function(){ //IIFE
-	
-	//model - name clickcount - img url
-	var model = {
-        catsId : 0,
-		cats : [],
-        
-        init: function(){
-            if(this.cats.length == 0){
-                var newId = model.catsId;
-                this.cats.push({
-                    id : newId,
-                    image : './images/cat1.jpg',
-                    name : 'Max',
-                    clicks : 0
-                });
-                ++newId;
-                this.cats.push({
-                    id : newId,
-                    image : './images/cat2.jpg',
-                    name : 'Juni',
-                    clicks : 0
-                });
-                ++newId;
-                this.cats.push({
-                    id : newId,
-                    image : './images/cat3.jpg',
-                    name : 'Toby',
-                    clicks : 0
-                });
-            } //else case: eventually accept new cat input
-        },
-        
-        getAllCats : function() {
-            return this.cats;
-        },
-        
-        getCat : function(id){
-            return this.cats[id];
-        }
-        
-	};
-	
-	//2 views - cat list - and cat image w clicks
-	var listView = {
-		init : function() {
-            //get list via id using jquery
-            this.catList = $('#catList');
-            listView.render();
-        },
-        
-        render : function(){
-            var htmlStr = '';
+/*~~~~~~~~~~~~~Model ~~~~~~~~~~~~~*/
+var model = {
+    catsId : 0,
+    currentCat : null,
+    cats : [],
 
-            controller.getCats().forEach(function(cat){
-                                            htmlStr += '<li>'+cat.name+'<br>'+'clicks: '+cat.clicks+'<button id='+cat.id+'>View Detail</button></li>';
-                                         });
-
-           this.catList.html(htmlStr);
-           controller.getCats().forEach(function(cat){
-                            this.button = $('#'+cat.id);
-                            this.button.click(function(){
-                                                controller.getSingleCat(cat);
-                                                });
-                                        });
-        }  
-    };
-    
-    var detailView = {
-        init : function(cat){
-            this.catDetail = $('#catDetail');
-            detailView.render(cat);
-        },
-        
-        render : function(cat){
-            var htmlStr = '';
-            htmlStr +=   '<img id='+cat.id+'img' +' src='+cat.image+'>'+'<br>';
-            this.catDetail.html(htmlStr);
-            this.catDetailImg = $('#'+cat.id+'img');
-            this.catDetailImg.click(function(){
-               controller.setClick(cat.id);
+    init: function(){
+        if(this.cats.length == 0){
+            var newId = model.catsId;
+            this.cats.push({
+                id : newId,
+                image : './images/cat1.jpg',
+                name : 'Max',
+                clicks : 0
             });
-            this.renderClick(cat.clicks);
-        },
-        
-        renderClick : function(clickCount){
-            this.catClicks = $('#catClicks');
-            this.catClicks.html(clickCount);
+            ++newId;
+            this.cats.push({
+                id : newId,
+                image : './images/cat2.jpg',
+                name : 'Juni',
+                clicks : 0
+            });
+            ++newId;
+            this.cats.push({
+                id : newId,
+                image : './images/cat3.jpg',
+                name : 'Toby',
+                clicks : 0
+            });
+           this.currentCat = this.cats[0];
+        } //else case: eventually accept new cat input
+    },
+
+    getAllCats : function() {
+        return this.cats;
+    },
+
+    getCat : function(id){
+        return this.cats[id];
+    }
+
+};
+
+
+/*~~~~~~~~~~~~~Views ~~~~~~~~~~~~~*/
+var listView = {
+    init : function() {
+        this.catListElem = document.getElementById('catList');
+        this.render();
+    },
+
+    render : function(){
+        var cat, elem, i;
+
+        this.catListElem.innerHTML = '';
+        var cats = controller.getCats();
+
+        for(i = 0; i < cats.length; i++){
+            cat = cats[i];
+            elem = document.createElement('li');
+            elem.textContent = cat.name;
+
+            elem.addEventListener('click',(function(catCopy){
+                return function(){
+                    controller.setCurrentCat(catCopy);
+                    detailView.render();
+                };
+            })(cat));
+            this.catListElem.appendChild(elem);
         }
-        
-    };
-		
-	//controller -init model, tell view to render
-		//method that increases the counter in the model, then in the view and rerenders
-	var controller = {
-        init : function() {
-            model.init();
-            listView.init();
-        },
-        
-        getCats : function(){
-            return model.getAllCats();
-        },
-        
-        getSingleCat : function(cat){
-            detailView.init(cat);
-        },
-        
-        setClick : function(id){
-            var cat = model.getCat(id);
-            detailView.renderClick(cat.clicks++);
-        }
-    };
-    
-    //Run first
-    controller.init();
-    
-});
+    }  
+};
+
+var detailView = {
+    init : function(){
+        //Cat detail div
+        this.catElem = document.getElementById('catDetail')
+        this.catNameElem = document.getElementById('catName');
+        this.catImgElem = document.getElementById('catImg');
+        this.catCountElem = document.getElementById('catCount');
+
+        //register clicker
+        this.catImgElem.addEventListener('click', function(){
+            controller.setClick();
+        })
+        this.render();
+    },
+
+    render : function(){
+        var currentCat = controller.getCurrentCat();
+        this.catCountElem.textContent = currentCat.clicks;
+        this.catNameElem.textContent = currentCat.name;
+        this.catImgElem.src = currentCat.image;
+    }
+
+};
+
+
+/*~~~~~~~~~~~~~Controller~~~~~~~~~~~~~*/
+var controller = {
+    init : function() {
+        model.init();
+        listView.init();
+        detailView.init();
+    },
+
+    getCats : function(){
+        return model.getAllCats();
+    },
+
+    getCurrentCat : function(){
+        return model.currentCat;
+    },
+
+    setCurrentCat : function(cat){
+      model.currentCat = cat;  
+    },
+
+    setClick : function(){
+        model.currentCat.clicks++;
+        detailView.render();
+    }
+};
+
+//Run first
+controller.init();
